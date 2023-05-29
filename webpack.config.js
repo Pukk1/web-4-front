c92aa369da3645c4dcb378dd60095a929deb7980
Ivan Kustarev <ivan.kustarev@tune-it.ru>
2023-05-29 15:39:29 +0300

    init ts app

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,16 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: "./src/index.tsx",
     mode: "development",
     module: {
         rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                options: {presets: ["@babel/env"]}
+            },
             {
                 test: /\.(js|jsx)$/,
                 exclude: /(node_modules|bower_components)/,
@@ -18,7 +24,7 @@ module.exports = {
             }
         ]
     },
-    resolve: {extensions: ["*", ".js", ".jsx"]},
+    resolve: {extensions: [".ts", ".tsx", ".js", ".jsx"]},
     output: {
         path: path.resolve(__dirname, "dist/"),
         publicPath: "/dist/",
