f63c3ec7cea273bcc3bd69da555f603843f950c3
Ivan Kustarev <ivan.kustarev@tune-it.ru>
2023-05-31 00:02:47 +0300

    working single page ssr

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,14 @@
 // server/index.js
 
-require( './express.js' );
+const path = require( 'path' );
+
+// игнорируем импорты `.scss`
+require( 'ignore-styles' );
+
+// транспилируем на лету импорты
+require( '@babel/register')( {
+    configFile: path.resolve( __dirname, './babel.config.js' ),
+} );
+
+// импортируем express-сервер
+require( './express' );
